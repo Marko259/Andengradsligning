dedba58df7261001930b2e0ba613ec7e5bea400d
Markus Nielsen <animationsisnice@gmail.com>
2022-02-05 21:22:34 +0100

    Added comments

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -2,22 +2,25 @@ $(document).ready(function () {
     $("#calculate").click(function () {
         let root1, root2;
 
+        // definer og hent værdier af a, b og c
         let a = $("#valueA").val();
         let b = $("#valueB").val();
         let c = $("#valueC").val();
 
+        // definer diskriminanten
         let discriminant = b * b - 4 * a * c;
 
+        // hvis diskriminanten er større end 0, så finder vi de to faktorer
         if (discriminant < 0) {
             root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
             root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
             displayResult(root1, root2);
-
-        } else if (discriminant == 0) {
+            
+        } else if (discriminant == 0) { // hvis diskriminanten er lig med 0, så finder vi kun en faktor
             root1 = root2 = -b / (2 * a);
             displayResult(root1, root2);
-        } else {
+        } else { // hvis diskriminanten er mindre end 0, så beregner vi løsningerne ved brug af formel 3 og 4.
             let realPlan = (-b / (2 * a)).toFixed(2);
             let imaginaryPart = (Math.sqrt(discriminant) / (2 * a)).toFixed(2);
 
@@ -28,6 +31,7 @@ $(document).ready(function () {
         }
     });
 
+    // vis resultatet
     function displayResult(root1, root2) {
         if (isNaN(root1) || isNaN(root2)) {
             $("#result").html("<p class='text-danger'>Enten er dette ikke en andengradsligning ellers findes der ingen løsning på dette.</p>");
@@ -36,6 +40,7 @@ $(document).ready(function () {
         }
     }
 
+    // nulstil
     $("#reset").click(function () {
         $("#valueA").val("");
         $("#valueB").val("");
